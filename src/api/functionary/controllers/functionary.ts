96e2b731414a46f81186a2d74f2cd35e5d786928
Fontinelle <epitaciofontinelle@live.com>
2022-11-03 15:44:48 -0300

    feat: adicionando a tabela de funcionários

---
+++ src/api/functionary/controllers/functionary.ts
@@ -0,0 +1,7 @@
+/**
+ * functionary controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::functionary.functionary');
---
+++ src/api/functionary/services/functionary.ts
@@ -0,0 +1,7 @@
+/**
+ * functionary service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::functionary.functionary');
