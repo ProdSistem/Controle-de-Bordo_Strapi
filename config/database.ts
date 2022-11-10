16176e234f4b580cfba812effb3270e2c2eeadab
Fontinelle <epitaciofontinelle@live.com>
2022-11-10 09:22:49 -0300

    refactor: atualização do strapi

--- config/database.ts
+++ config/database.ts
@@ -2,12 +2,12 @@ export default ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL'),
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'test'),
+      user: env('DATABASE_USERNAME', 'test'),
+      password: env('DATABASE_PASSWORD', '123456'),
+      ssl: env.bool('DATABASE_SSL', false),
     },
   },
 });
