1defb717034bc8375079b94668483f0cd9372d11
Fontinelle <epitaciofontinelle@live.com>
2022-11-19 10:22:52 -0300

    build: alterando banco de dados para postgres

--- config/database.ts
+++ config/database.ts
@@ -1,12 +1,12 @@
 export default ({ env }) => ({
   connection: {
-    client: 'mysql',
+    client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'test'),
-      user: env('DATABASE_USERNAME', 'test'),
-      password: env('DATABASE_PASSWORD', '123456'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
