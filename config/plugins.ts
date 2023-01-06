144492305dee64610bd91a9e3b868406cb33011c
Fontinelle <epitaciofontinelle@live.com>
2023-01-06 09:27:06 -0300

    feat: adicionando configurações de e-mail com nodemailer

---
+++ config/plugins.ts
@@ -0,0 +1,19 @@
+export default ({ env }) => ({
+  email: {
+    config: {
+      provider: 'nodemailer',
+      providerOptions: {
+        host: env('SMTP_HOST'),
+        port: env('SMTP_PORT'),
+        auth: {
+          user: env('SMTP_USERNAME'),
+          pass: env('SMTP_PASSWORD'),
+        },
+      },
+      settings: {
+        defaultFrom: env('SMTP_FROM'),
+        defaultReplyTo: env('SMTP_REPLAY_TO'),
+      },
+    },
+  },
+});
