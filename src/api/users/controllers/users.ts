27659c216f22db98a8172a54304cf166d2366994
Fontinelle <epitaciofontinelle@live.com>
2023-01-09 18:47:25 -0300

    refactor: adicionando filtros e paginação rota de usuários

--- src/api/users/controllers/users.ts
+++ src/api/users/controllers/users.ts
@@ -3,19 +3,23 @@ export default {
     try {
       const { page, pageSize } = ctx.query?.pagination || {
         pageSize: null,
-        offset: null,
         page: null,
       };
-      const offset = page * pageSize - pageSize;
-      const limit = pageSize;
       const { name, cpf, id, active } = ctx.query;
-      let data = '';
+
+      const start = page * pageSize - pageSize;
+      const limit = pageSize;
+
+      let users = [];
       let total = 1;
 
       if (name || cpf || id || active) {
-        data = await strapi.entityService.findMany(
+        users = await strapi.entityService.findMany(
           'plugin::users-permissions.user',
           {
+            start,
+            limit,
+            populate: '*',
             filters: {
               name: {
                 $containsi: name ? name : '',
@@ -30,9 +34,6 @@ export default {
                 $containsi: active ? active : '',
               },
             },
-            offset,
-            limit,
-            populate: '*',
           },
         );
 
@@ -53,17 +54,93 @@ export default {
           },
         });
       } else {
-        data = await strapi.entityService.findMany(
+        users = await strapi.entityService.findMany(
           'plugin::users-permissions.user',
-          { offset, limit },
+          { start, limit, populate: '*' },
         );
-
         total = await strapi.db
           .query('plugin::users-permissions.user')
           .count({});
       }
 
       const pageCount = pageSize ? Math.ceil(total / pageSize) : 1;
+
+      const data = [];
+      await users.map((user, key) => {
+        data[key] = {
+          id: user.id,
+          username: user.username,
+          email: user.email,
+          confirmed: user.confirmed,
+          blocked: user.blocked,
+          createdAt: user.createdAt,
+          updatedAt: user.updatedAt,
+          cpf: user.cpf,
+          status: user.status,
+          email_functional: user.email_functional,
+          name: user.name,
+          role: {
+            id: user.role?.id,
+            name: user.role?.name,
+            description: user.role?.description,
+            type: user.role?.type,
+            createdAt: user.role?.createdAt,
+            updatedAt: user.role?.updatedAt,
+          },
+          created_for: {
+            id: user.created_for?.id,
+            username: user.created_for?.username,
+            email: user.created_for?.email,
+            provider: user.created_for?.provider,
+            confirmed: user.created_for?.confirmed,
+            blocked: user.created_for?.blocked,
+            createdAt: user.created_for?.createdAt,
+            updatedAt: user.created_for?.updatedAt,
+            cpf: user.created_for?.cpf,
+            status: user.created_for?.status,
+            email_functional: user.created_for?.email_functional,
+            name: user.created_for?.name,
+          },
+          updated_for: {
+            id: user.updated_for?.id,
+            username: user.updated_for?.username,
+            email: user.updated_for?.email,
+            provider: user.updated_for?.provider,
+            confirmed: user.updated_for?.confirmed,
+            blocked: user.updated_for?.blocked,
+            createdAt: user.updated_for?.createdAt,
+            updatedAt: user.updated_for?.updatedAt,
+            cpf: user.updated_for?.cpf,
+            status: user.updated_for?.status,
+            email_functional: user.updated_for?.email_functional,
+            name: user.updated_for?.name,
+          },
+          createdBy: {
+            id: user.createdBy?.id,
+            firstname: user.createdBy?.firstname,
+            lastname: user.createdBy?.lastname,
+            username: user.createdBy?.username,
+            email: user.createdBy?.email,
+            isActive: user.createdBy?.isActive,
+            blocked: user.createdBy?.blocked,
+            preferedLanguage: user.createdBy?.preferedLanguage,
+            createdAt: user.createdBy?.createdAt,
+            updatedAt: user.createdBy?.updatedAt,
+          },
+          updatedBy: {
+            id: user.updatedBy?.id,
+            firstname: user.updatedBy?.firstname,
+            lastname: user.updatedBy?.lastname,
+            username: user.updatedBy?.username,
+            email: user.updatedBy?.email,
+            isActive: user.updatedBy?.isActive,
+            blocked: user.updatedBy?.blocked,
+            preferedLanguage: user.updatedBy?.preferedLanguage,
+            createdAt: user.updatedBy?.createdAt,
+            updatedAt: user.updatedBy?.updatedAt,
+          },
+        };
+      });
 
       ctx.body = {
         data,
